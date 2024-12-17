2370bbc6d0e90f47544b6f623bf2733f5a612083
Diego Ferrer Rojas <145705307+Garlisio@users.noreply.github.com>
2024-12-17 18:29:52 +0000

    Delete CardCalculator directory

--- CardCalculator/src/main/java/com/diego/crud/entity/dao/AnalysisDao.java
+++
@@ -1,8 +0,0 @@
-package com.diego.crud.entity.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.diego.crud.entity.models.Analysis;
-
-public interface AnalysisDao extends CrudRepository<Analysis, Integer> {
-}
